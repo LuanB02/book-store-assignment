ec22f89b3a1fb93129b7593fa6438b7f0794050d
LuanBui02 <luanblp@teamsolutions.vn>
2024-04-04 17:00:06 +0700

    update view

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -1,5 +1,25 @@
+import Container from "../Container";
+import image from "../../assets/image.png";
+
+import Header from "../Header";
+import Footer from "../Footer";
 const Dashboard = () => {
-  return <section className="new-property">Hello</section>;
+  return (
+    <section
+      className="new-property h-[700px] bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${image})` }}
+    >
+      <div>
+        <Header />
+      </div>
+      <div className="mt-[300px]">
+        <Container />
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
+    </section>
+  );
 };
 
 export default Dashboard;
