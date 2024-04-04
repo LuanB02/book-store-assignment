975fba6099ed9a451e137321ef630647527684dc
LuanBui02 <luanblp@teamsolutions.vn>
2024-04-04 16:58:09 +0700

    add layout container

---
+++ src/views/Container.tsx
@@ -0,0 +1,15 @@
+import img1 from "../assets/Bookshop.gif";
+
+const Container = () => {
+  return (
+    <div>
+      <img
+        className="items-center justify-center m-auto"
+        src={img1}
+        alt="bookShop"
+      />
+    </div>
+  );
+};
+
+export default Container;
